6ce5e31b15c97f0a3c6655dbb19592f2f9a810d1
Dr Alun Moon <alun.moon@northumbria.ac.uk>
2019-12-02 21:01:38 +0000

    Skeleton mini project

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* KF6010 --- Distributed Real-time Systems
+    Skeleton program for the mini-project
+	End of Semester One
+*/
+
+#include <mbed.h>  /* All you need for the mbed API and OS */
+
+/* Some useful documentation pages:
+      Semaphore    https://os.mbed.com/docs/mbed-os/v5.14/apis/semaphore.html
+      Mutex        https://os.mbed.com/docs/mbed-os/v5.14/apis/mutex.html
+
+      InterruptIn  https://os.mbed.com/docs/mbed-os/v5.14/apis/interruptin.html
+*/
+
+
+int main(void)
+{
+}
+
